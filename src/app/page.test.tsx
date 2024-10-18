0db8321fb01d2b0b0b7d4214e3dccee4c5937e1c
Chris Bendel <chrissbendel@gmail.com>
2024-10-18 07:56:37 -0400

    Page test comments

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,13 +1,9 @@
-import { expect, describe, it } from 'vitest'
-import { render, within } from '@testing-library/react'
-import Page from './page'
-
-import { Providers as P } from '@/components/providers'
+import { describe, it } from 'vitest'
 
 describe('Main Page', () => {
     it('redirects when not signed in', async () => {
-        // const { getByRole } = render(<Page />, { wrapper: P })
-        // const main = within(getByRole('main'))
-        // expect(main).toBeDefined()
+        // TODO Write test for this at some point
+        //  Instead of redirecting, we use basic auth which just
+        //  has the built in popup
     })
 })
