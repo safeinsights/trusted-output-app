391c3d67ae21278ee23fa9a0a1583a370fd620df
Chris Bendel <chrissbendel@gmail.com>
2025-01-06 14:00:42 -0500

    Package json sorting out

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
+    plugins: [react(), tsconfigPaths()],
     test: {
         setupFiles: ['./tests/vitest.setup.ts'],
         mockReset: true,
