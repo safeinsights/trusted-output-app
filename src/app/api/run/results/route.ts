582c062f8f903de0da538a9cccc16e3d25ce83d3
Samuel Klutse <klutse.samuel@gmail.com>
2025-01-13 10:11:26 -0500

    shrimp-50: TOA Logging

--- src/app/api/run/results/route.ts
+++ src/app/api/run/results/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import fs from 'fs'
 import path from 'path'
-import { UPLOAD_DIR } from '@/app/utils'
+import { UPLOAD_DIR, log } from '@/app/utils'
 import { parse } from 'csv-parse/sync'
 
 // This disables caching for this endpoint
@@ -9,12 +9,13 @@ export const revalidate = 0
 
 export function GET() {
     const runs: Record<string, any[]> = {} // eslint-disable-line
-
+    log('Retrieving results from upload directory')
     if (fs.existsSync(UPLOAD_DIR)) {
         const files = fs.readdirSync(UPLOAD_DIR)
 
         for (const file of files) {
             const filePath = path.join(UPLOAD_DIR, file)
+            log(`Processing file: ${file}`)
             const fileContent = fs.readFileSync(filePath, 'utf-8')
 
             runs[file] = parse(fileContent, {
