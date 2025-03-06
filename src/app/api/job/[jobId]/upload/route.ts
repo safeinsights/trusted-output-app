acf31c46eecc2f406c59e2287df2f560ca324f75
KC Swanson <kendraswan@gmail.com>
2025-03-06 18:36:36 -0500

    encrypt results if job has public key(s)

--- src/app/api/job/[jobId]/upload/route.ts
+++ src/app/api/job/[jobId]/upload/route.ts
@@ -1,7 +1,8 @@
-import { isValidUUID, log, saveFile, UPLOAD_DIR } from '@/app/utils'
+import { isValidUUID, log, saveFile, UPLOAD_DIR, ensureValue, generateAuthorizationHeaders } from '@/app/utils'
 import fs from 'fs'
 import { NextRequest, NextResponse } from 'next/server'
 import path from 'path'
+import { encryptResults, getPublicKeys } from '@/encrypt'
 
 function isFile(obj: FormDataEntryValue): obj is File {
     return obj instanceof File
@@ -22,14 +23,43 @@ export const POST = async (req: NextRequest, { params }: { params: Promise<{ job
     } else if (Object.keys(body).length !== 1) {
         errorMessage = 'Form data includes unexpected data keys'
     }
-    const filePath = path.join(UPLOAD_DIR, jobId)
-    if (fs.existsSync(filePath)) {
-        errorMessage = 'Data already exists for jobId'
+
+    log("Getting public keys ...")
+    let publicKeys = await getPublicKeys(jobId)
+    publicKeys = ensureValue(publicKeys)
+    if (publicKeys.keys.length === 0) {
+        log('No public keys found for job ID: ' + jobId)
+        log('Legacy approval: Saving results in memory ...')
+        const filePath = path.join(UPLOAD_DIR, jobId)
+        if (fs.existsSync(filePath)) {
+            errorMessage = 'Data already exists for jobId'
+        }
+        if (errorMessage.length == 0 && 'file' in body && isFile(body.file)) {
+            await saveFile(body.file, jobId)
+            return NextResponse.json({}, { status: 200 })
+        }
+    } else {
+        log('Encrypting results with public keys ...')
+        const resultsBuffer = Buffer.from(await body.file.arrayBuffer())
+        const encryptedResults = await encryptResults(jobId, resultsBuffer, publicKeys.keys)
+        const formData = new FormData()
+        formData.append('file', new File([encryptedResults], jobId, { type: 'application/zip' }))
+        const endpoint = `${process.env.MANAGEMENT_APP_API_URL}/api/job/${jobId}/results`
+        log(`BMA: Uploading encrypted results ${endpoint}`)
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                ...generateAuthorizationHeaders(),
+            },
+        })
+        if (!response.ok) {
+            errorMessage = `Failed to post encrypted results: ${response.status}`
+        } else {
+            return NextResponse.json({}, { status: 200 })
+        }
     }
-    if (errorMessage.length == 0 && 'file' in body && isFile(body.file)) {
-        await saveFile(body.file, jobId)
-        return NextResponse.json({}, { status: 200 })
-    }
+
     log(errorMessage, 'error')
     return NextResponse.json({ error: errorMessage }, { status: 400 })
 }
--- src/encrypt.ts
+++ src/encrypt.ts
@@ -1,37 +1,46 @@
 import { ResultsWriter } from 'si-encryption/job-results/writer'
-import { GET } from './app/api/job/results/route'
+import { generateAuthorizationHeaders } from './app/utils'
 
-// TODO: look up private key in BMA corresponding to job
-// Using placeholders for now from https://cryptotools.net/rsagen
-const publicKeyPlaceholder1 = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCFStw21WlVg2eLXfmAj7lyCiEz
-1/OkJOL6K1CWGkrBAa2oP/wVzYHUomQaQQ0SDdQYx4EZZkfehIOoFUwb0B+Cka+F
-xGbqlSoEna0+yU6TWB2nPiIRYEya7OLY6V/tU0z8ECYL89uWU4W+IskAxpWvUUuH
-rFjDWd0FoH4+yOkOHQIDAQAB`
-const publicKeyPlaceholder2 = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCpGKeRCSeaQnu5pANdXPD+iY4
-YzMhGNIEH0VOyF61LAr8Qn3MIgU47opbAA6xPaU8jm/qpN3so10kfmct3ur1iQIJ
-W/xW57BZSlb7QqpOEm9Cwfv1yGCVx7KeqJOks51MrUupJZxgWfodWHLKwvxSywN8
-RMlHJ3wbr/mWrTW6qwIDAQAB`
-
-export const encryptResults = async () => {
-  const writer = new ResultsWriter([
-    { fingerprint: 'fingerprint1', publicKey: publicKeyPlaceholder1 },
-    { fingerprint: 'fingerprint2', publicKey: publicKeyPlaceholder2 },
-  ])
-
-  const results: { jobs: Record<string, any[]> } = await GET().json() // eslint-disable-line
-  const jobKeys = Object.keys(results.jobs)
-
-  for (const index in jobKeys) {
-    const jobId = jobKeys[index]
-
-    // Encrypt jobResults using ResultsWriter & private keys
-    const jobResults = results.jobs[jobId]
-    const jobResultsBuffer = new TextEncoder().encode(JSON.stringify(jobResults))
-    await writer.addFile(jobId, jobResultsBuffer)
-  }
-  // console.log(JSON.stringify(writer.manifest))
-  const encryptedResults: Blob = await writer.generate()
-  return encryptedResults
+type ManagementAppPublicKeyResponse = {
+    keys: ManagementAppPublicKey[]
 }
 
-// encryptResults().then((res) => console.log(res))
+type ManagementAppPublicKey = {
+    jobId: string, publicKey: string, fingerprint: string
+}
+
+export const getPublicKeys = async (jobId: string): Promise<ManagementAppPublicKeyResponse | undefined> => {
+    const endpoint = `${process.env.MANAGEMENT_APP_API_URL}/api/job/${jobId}/keys`
+    const headers = generateAuthorizationHeaders()
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json',
+        },
+    })
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch public keys for job ID: ${jobId}`)
+    }
+
+    try {
+        const body = await response.json()
+        return body
+    } catch (e) {
+        console.error('Failed to parse response body', e)
+    }
+}
+
+export const encryptResults = async (jobId: string, jobResults: ArrayBuffer, publicKeys: ManagementAppPublicKey[]): Promise<Blob> => {
+    const writerParams = publicKeys.map((key) => {
+        return { fingerprint: key.fingerprint, publicKey: key.publicKey }
+    })
+    const writer = new ResultsWriter(writerParams)
+
+    await writer.addFile(jobId, jobResults)
+
+    const encryptedResults: Blob = await writer.generate()
+    return encryptedResults
+}
